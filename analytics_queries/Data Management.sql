bd9eb9521f130032a0b31db9b70102e08db21553
Peter Garay-Robles <peter.garayrobles@gmail.com>
2026-02-28 18:30:09 -0500

    Update Data Management.sql

--- analytics_queries/Data Management.sql
+++ analytics_queries/Data Management.sql
@@ -18,7 +18,7 @@ WHERE title LIKE '____';    -- 4 underscores indicate 4 characters
 
 --------------------------------------------------------------------
 
---9 return the rows with the second to fourth highest price 
+-- return the rows with the second to fourth highest price 
 ORDER BY price DESC
 OFFSET 1
 LIMIT 3;
@@ -26,7 +26,7 @@ LIMIT 3;
 
 
 
---10 for each vendor_name: ->combine the vendor_city and vendor_state
+-- for each vendor_name: ->combine the vendor_city and vendor_state
 SELECT
 vendor_name,
   
@@ -37,7 +37,7 @@ LIMIT 3;
 
 
 
---11 return the number of rows that the speaker's price is higher than the average price from the speaker table.
+-- return the number of rows that the speaker's price is higher than the average price from the speaker table.
 SELECT
 COUNT(*) AS above_avg
 FROM speaker
@@ -45,7 +45,7 @@ WHERE price > (SELECT AVG(price) FROM speaker);  -- Wrong -> Aggregate functions
 
 
 
---12 Return the difference between each month's highest and lowest price.
+-- Return the difference between each month's highest and lowest price.
 SELECT
 EXTRACT(month FROM date::DATE) AS month,
 MAX(price) - MIN(price) AS difference
@@ -53,7 +53,7 @@ MAX(price) - MIN(price) AS difference
 
 ---------------------------------------------------------------
   
---14 bike_stations contains newly built bike locations. bike_trips contains bike trips on new locations or previously existing ones. return each bike trip only started in newly built bike stations.
+-- bike_stations contains newly built bike locations. bike_trips contains bike trips on new locations or previously existing ones. return each bike trip only started in newly built bike stations.
 SELECT
 trip_id,
 station_id,
@@ -65,7 +65,7 @@ on t.stating_station = s.station_id;
 -- The explanation is because a left join will return NULL for columns where there is no match with the right side. An inner join will return only matched records. 
 
 
---4 Join table but keep all the records no mater whether they a match
+-- Join table but keep all the records no mater whether they a match
 SELECT
   trip_id,
   duration,
@@ -78,7 +78,7 @@ SELECT
 
 --------------------------------------------------------------------------------------------
 
-  --15 return number of duplicates records
+-- return number of duplicates records
 SELECT
 name,
 city,
@@ -88,7 +88,7 @@ FROM vendors
 GROUP BY name, city, state    --by grouping by these and selecting the count it shows dups
 
 
---14 Return vendors from austin city
+-- Return vendors from austin city
 SELECT
 vendor_name, 
 vendor_city,
@@ -97,14 +97,14 @@ FROM vendors
 WHERE vendor_city = 'AUSTIN';
 
 
---13 Return the number of rows in the 'launch' column where the value is NOT a date OR the value is missing
+-- Return the number of rows in the 'launch' column where the value is NOT a date OR the value is missing
 SELECT 
 COUNT(*)
 FROM speaker
 WHERE launch = 'Null' OR launch IS NULL;    --Not a date is labeled 'Null'
 
 
---12 Validate the date and return the rows that the 'installation_date' is not a date in 2013
+-- Validate the date and return the rows that the 'installation_date' is not a date in 2013
 SELECT *
 FROM station
 WHERE installation_date NOT BETWEEN '2013-01-01' 
@@ -112,13 +112,13 @@ WHERE installation_date NOT BETWEEN '2013-01-01'
 AND '2013-12-31';
 
 
---11 Validate the date there should not be any negative values
+-- Validate the date there should not be any negative values
 SELECT *
 FROM trips
 WHERE duration < 0;
 
 
---7 Join the table with another but keep all the records from one table no matter whether they have a match in the other
+-- Join the table with another but keep all the records from one table no matter whether they have a match in the other
 SELECT trip_id,
 duration,
 t.bike_id
@@ -127,7 +127,7 @@ LEFT JOIN bike as b
 ON t.bike_id = b.bike_id;
 
 
---6 Join the tables but keep only the matched records
+-- Join the tables but keep only the matched records
 SELECT trip_id,
 duration,
 t.bike_id
@@ -136,7 +136,7 @@ INNER JOIN bike as b
 ON t.bike_id = b.bike_id;
 
 
---5 Join the table with itself to return the pairs of the speaker model with the same launch date
+-- Join the table with itself to return the pairs of the speaker model with the same launch date
 SELECT
 s1.model, 
 s2.model,
@@ -148,14 +148,14 @@ ON s1.productid <> s2.productid
 AND s1.launch = s2.launch;
 
 
---3 Return the day from the column and convert the day to numeric data type
+-- Return the day from the column and convert the day to numeric data type
 SELECT
 trip_id,
 EXTRACT(DAY FROM start_time) :: NUMERIC AS start_day
 FROM bike_trips;
 
 
---2 Return the first five rows that the title DOES NOT contain 'the' no matter the case of the string
+-- Return the first five rows that the title DOES NOT contain 'the' no matter the case of the string
 SELECT *
 FROM movie_budget
   
@@ -163,7 +163,7 @@ WHERE title NOT ILIKE '%the%'
 LIMIT 5;
 
 
---1 Validate the data by returning the rows that the 'bike_available' column is out of range (each station has at least one bike and at most 10)
+-- Validate the data by returning the rows that the 'bike_available' column is out of range (each station has at least one bike and at most 10)
 SELECT *
 FROM status
 WHERE bikes_available < 1 
@@ -250,9 +250,6 @@ limit 3;
 -- Where are the bike stations available in the city?
 
 
-
-
-
 --------------------------------------------------------------------------
 
 -- Return the Rows that only appears in the movie_2000, But Not movie_2010
@@ -269,4 +266,3 @@ ORDER BY year;
 -----------------------------
 
 -- Created on 7.31.2025
-
