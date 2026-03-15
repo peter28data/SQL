1050f15a502545fde82f5c15188064a7f2de343e
Peter Garay-Robles <peter.garayrobles@gmail.com>
2026-03-14 23:28:46 -0400

    Update Date Formats.sql

--- etl_transformations/Date Formats.sql
+++ etl_transformations/Date Formats.sql
@@ -2,35 +2,6 @@
 
 -- ETL Transformations require preparation of date and timestamp data types. Below are the various techniques necessary to prepare these data types.
 
-----------------------------------------------------------
-
--- Date Comparisons
--- The format will return either a 0 or 49 requests. 
-
-SELECT COUNT(*)
-FROM evanston311
-WHERE date_created = '2018-01-01';
-
-
--- The query above will not return anything. However, when we convert the query to a timestamp it will return 49 requests.
-WHERE date_created = '2018-01-02'::timestamp;
-
-
-----------------------------------------------------------
-
--- Number of Requests
-SELECT count(*) 
-FROM evanston311
-WHERE date_created::DATE = '2017-01-31';
-
--- Number of Requests
--- Greater than or equal to Feb 29 but less than March 1st queries 
-SELECT count(*)
-FROM evanston311
-WHERE date_created >= '2016-02-29'
-AND date_created < '2016-03-01';
-
-
 ---------------------------------------------------------
 
 -- Specify the Upper Bound
