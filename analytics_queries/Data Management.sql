c789155a37af4cc91cb955b362e03d664d208eb3
Peter Garay-Robles <peter.garayrobles@gmail.com>
2026-07-26 23:36:23 -0400

    Update Data Management.sql

--- analytics_queries/Data Management.sql
+++ analytics_queries/Data Management.sql
@@ -1,6 +1,8 @@
 -----------------------------------------------------------------------
 
--- return rows where the title contains 'k' or 'm' only in lowercase
+-- Regex Tradeoffs 
+-- Returns Rows where the Title contains 'k' or 'm' only in lowercase
+
 WHERE title SIMILAR TO '%(k|m)%';    -- LIKE can be used for lowercase specific character
 
 -- the LIKE operator does not support Regular expressions or pattern alternation using Parentheses and the pipe symbol ( (k|m) ).
